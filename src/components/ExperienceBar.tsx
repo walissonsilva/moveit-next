863ff7969d1b65f566e210df653405a587b9f803
Walisson Silva <walissonsilva10@gmail.com>
2021-02-23 10:43:57 -0300

    chore: home container design completed (left-side)

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from '../styles/components/ExperienceBar.module.css';
+
+const ExperienceBar: React.FC = () => {
+  return (
+    <header className={styles.experienceBar}>
+      <span>0 xp</span>
+      <div>
+        <div style={{ width: '50%' }}></div>
+        <span className={styles.currentExperience} style={{ left: '50%' }}>
+          300 xp
+        </span>
+      </div>
+      <span>600 xp</span>
+    </header>
+  )
+}
+
+export default ExperienceBar;
